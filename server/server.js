2e0cedb93e54c81950791ee6a3b1b97a31f3bff6
Pedro <pefernandez1408@gmail.com>
2020-06-26 15:36:34 -0430

    Modelo de Usuario

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 require('./config/config')
 const express = require('express')
 const app = express()
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
 
 const bodyParser = require('body-parser')
@@ -15,13 +15,13 @@ app.use(bodyParser.json())
 app.use( require('./routes/usuario') ); //conexion a hacia las rutas 
 
  
-  mongoose.connect('mongodb://localhost:27017/tienda', (err, res) => {
+  // mongoose.connect('mongodb://localhost:27017/tienda', (err, res) => {
       
-   if(err) throw err;
+  //  if(err) throw err;
 
-   console.log('Base de datos ONLINE');
+  //  console.log('Base de datos ONLINE');
 
-  });
+  // });
 
   app.listen(process.env.PORT, () => {
     console.log(`Escuchando el puerto: ${process.env.PORT}`);
