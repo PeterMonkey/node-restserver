2653e5cf9df1e0b587bfaa3b6db64a8eb42c38e8
Pedro <pefernandez1408@gmail.com>
2020-07-15 21:29:59 -0400

    Conectando la BD-2

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/tienda'
 } else {
-   urlDB = 'mongodb://strike:FQX0zt7hNJYJr3IR@cluster0-shard-00-00.5ldh1.mongodb.net:27017,cluster0-shard-00-01.5ldh1.mongodb.net:27017,cluster0-shard-00-02.5ldh1.mongodb.net:27017/tienda?ssl=true&replicaSet=atlas-sl9qhj-shard-0&authSource=admin&retryWrites=true&w=majority'
+   urlDB = 'mongodb://strike:FQX0zt7hNJYJr3IR@cluster0-shard-00-00.5ldh1.mongodb.net:27017,cluster0-shard-00-01.5ldh1.mongodb.net:27017,cluster0-shard-00-02.5ldh1.mongodb.net:27017/test?replicaSet=atlas-sl9qhj-shard-0&ssl=true&authSource=admin'
 }
 
 process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json())
 app.use( require('./routes/usuario') ); //conexion a hacia las rutas 
 
  
-  mongoose.connect(process.env.URLDB, {useNewUrlParser: true, useCreateIndex: true})
+  mongoose.connect('mongodb://strike:FQX0zt7hNJYJr3IR@cluster0-shard-00-00.5ldh1.mongodb.net:27017,cluster0-shard-00-01.5ldh1.mongodb.net:27017,cluster0-shard-00-02.5ldh1.mongodb.net:27017/test?replicaSet=atlas-sl9qhj-shard-0&ssl=true&authSource=admin', {useNewUrlParser: true, useCreateIndex: true})
             .then(mongoose => console.log('BD en linea'))
             .catch(err => console.log(err));
 
